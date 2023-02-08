f32dababbded279316b26727ef04e664f4db2ac8
milani <i.milani@dasa.com.br>
2023-02-08 09:45:36 -0300

    feat: delete button created

--- app/myscripts.js
+++ app/myscripts.js
@@ -1,16 +1,27 @@
 class Book {
   constructor(book, author, read) {
+    this.id = self.crypto.randomUUID();
     this.book = book;
     this.author = author;
     this.read = read;
   }
   anunciarTitulo() {
-    console.log(`The book ${this.book} authored by ${this.author} is ${this.read}`);
+    console.log(
+      `The book ${this.book} authored by ${this.author} is ${this.read}`
+    );
     return `The book ${this.book} authored by ${this.author} is ${this.read}`;
   }
 }
 
 let list = [];
+
+const idades = [10, 22, 42, 16, 50];
+const podeDirigir = idades.filter((idade) => {
+  return idade >= 18;
+});
+
+console.log(podeDirigir);
+console.log(idades);
 
 function submitForm(event) {
   event.preventDefault();
@@ -47,6 +58,19 @@ function submitForm(event) {
   renderBookList();
 }
 
+function deleteBook(id) {
+  console.log("antes do filtro", list);
+  list = list.filter((value) => {
+    if (value.id == id) {
+      return false;
+    }
+    return true;
+  });
+  console.log("depois do filtro", list);
+
+  renderBookList();
+}
+
 function renderBookList() {
   let listDiv = document.getElementById("list");
 
@@ -59,6 +83,27 @@ function renderBookList() {
 
     listDiv.append(currentBook.anunciarTitulo());
 
+    listDiv.innerHTML +=
+      "<button type='button' class='delete' onclick='deleteBook(\"" +
+      currentBook.id +
+      "\")'>Delete</button>";
+
     listDiv.innerHTML += "<hr/>";
   }
+  const livrosNaoLidos = list.filter((currentLivro) => {
+    return currentLivro.read == "no read";
+  });
+
+  console.log(livrosNaoLidos);
+
+  listDiv.append("Falta a leitura de " + livrosNaoLidos.length + " livro(s).");
+
+  const livrosLidos = list.filter((currentLivro) => {
+    return currentLivro.read == "read";
+  });
+
+  console.log(livrosLidos);
+  listDiv.innerHTML += "<br>";
+
+  listDiv.append("Você já leu " + livrosLidos.length + " livro(s).");
 }
